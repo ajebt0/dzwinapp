28deedd40e0aeeb1c6754cfa7ce30b2451c48eee
ajebt0 <birtr1923@gmail.com>
2026-06-24 15:01:26 +0300

    Add files via upload

---
+++ dz 10/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string text = File.ReadAllText(openFileDialog.FileName);
+                textBox.Text = text;
+            }
+        }
+    }
+}
